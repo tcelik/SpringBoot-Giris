8e9110590f96d16fd32f41634a660b578bfdf57d
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-03-14 01:43:08 +0300

    004-ÇözünürlükOperatörüAppRunnerKullanımı

--- 004-SimpleCalculatorComponent/src/main/java/org/csystem/sample/App.java
+++ 004-SimpleCalculatorComponent/src/main/java/org/csystem/sample/App.java
@@ -2,10 +2,14 @@ package org.csystem.sample;
 
 
 import org.csystem.sample.calculator.operation.SimpleCalculator;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class App {
@@ -15,14 +19,31 @@ public class App {
     }
 
     @Bean
-    public ApplicationRunner run(SimpleCalculator calculator)
+    public ApplicationRunner run1(SimpleCalculator calculator)
     {
+        System.out.println(calculator.toString());
         return args -> {
             calculator.calculate('+', 10, 10);
             calculator.calculate('*', 10, 10);
         };
     }
+
+    @Bean
+    public ApplicationRunner run2(SimpleCalculator calculator)
+    {
+        System.out.println(calculator.toString());
+
+        return Util::run;
+    }
 }
+
+class Util {
+    public static void run(ApplicationArguments args)
+    {
+        Arrays.stream(args.getSourceArgs()).forEach(s -> System.out.println(s));
+    }
+}
+
 
 
 
